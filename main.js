825fc6c698043e433628584125146a6b4094cd2b
Fanomezana <rabevasoa.fanomezana@gmail.com>
2024-02-29 08:09:37 +0300

    get even values

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 console.log("test");
-function som(a,b){
-   return a+b
+function som(a, b) {
+	return a + b;
 }
 
-som(2,4)
+som(2, 4);
+
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function getEvenValue(array) {
+	return array.filter((value) => value % 2 == 0);
+}
+
+console.log(getEvenValue(data));
